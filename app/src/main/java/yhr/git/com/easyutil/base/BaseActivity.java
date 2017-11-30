a24754c38b8a16e3d2638473999e09cbbbdf5cbe
yanhairong <1204234297@qq.com>
2017-11-30 20:54:27 +0800

    Initial commit

---
+++ app/src/main/java/yhr/git/com/easyutil/base/BaseActivity.java
@@ -0,0 +1,22 @@
+package yhr.git.com.easyutil.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description:
+ *
+ * @author YanHaiRong
+ * @version 2.0
+ * @since 2017/11/20
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        
+    }
+}
